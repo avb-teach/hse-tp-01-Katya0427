780ba32ab05aeec2787f3c54fbc1f9978eaadda1
Katya0427 <kate.peganova@mail.ru>
2025-04-29 18:25:02 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -8,6 +8,7 @@ else max_depth=""
 fi
 
 if [[ -z "$max_depth" ]]
-then find "$input_dir" -type f -exec cp {} "$output_dir" \;
-else find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
+then find "$input_dir" -type f -exec cp {} "$output_dir"
+else find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir"
+     find "$input_dir" -maxdepth "$max_depth" -type d -exec cp -r {} "$output_dir"
 fi
