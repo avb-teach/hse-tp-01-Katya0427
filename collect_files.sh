9c6df96307d5513ebcf4b83b2a0d19d110396a08
Katya0427 <kate.peganova@mail.ru>
2025-04-29 19:38:04 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -2,6 +2,7 @@
 
 input_dir=$1
 output_dir=$2
+
 if  [[ "$#" -eq 3 ]]
 then max_depth=$3
 else max_depth=""
@@ -9,6 +10,18 @@ fi
 
 if [[ -z "$max_depth" ]]
 then find "$input_dir" -type f -exec cp {} "$output_dir" \;
-else find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
-     find "$input_dir" -maxdepth "$max_depth" -type d -exec cp -r {} "$output_dir" \;
+else 
+     current_depth=1
+     while [[ $current_depth -le $max_depth ]]; do
+          for item in "$input_dir"/*; do
+             if [[ -d "$item" ]]
+             then dir_name=$(basename "$item")
+                  mkdir -p "$output_dir/$dir_name"
+             elif [[ -f "$item" ]]
+             then cp "$item" "$output_dir/"
+             fi
+             done
+          input_dir="$input_dir/*"
+          current_depth=$((current_depth + 1))
+        done
 fi
