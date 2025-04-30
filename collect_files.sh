0660d26d5a30f480a146de4c95ebf0607c6af1e8
Katya0427 <kate.peganova@mail.ru>
2025-04-30 19:58:40 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -3,25 +3,36 @@
 input_dir=$1
 output_dir=$2
 
-if  [[ "$#" -eq 3 ]]
-then max_depth=$3
+if  [[ "$#" -eq 3 ]]; then
+     max_depth=$3
 else max_depth=""
 fi
 
+copy(){
+     current_dir=$1
+     current_depth=$2
+     if [[ "$current_depth" -gt "$max_depth" ]]; then 
+          return
+     fi
+     
+     for item in "$current_dir"/*; do
+          if [[ -d "$item" ]]; then
+               copy "$item" $((current_depth + 1))
+          elif [[ -f "$item" ]]; then
+               first_part=$(basename "$item")
+               name="$first_part"
+               count=1
+               while [[ -e "$output_dir/$name" ]]; do
+                    name="{first_part%.*}-$count.${name##*.}"
+                    count=$((count + 1))
+               done
+               cp "$item" "$output_dir/$name"
+          fi
+     done
+}                
+     
 if [[ -z "$max_depth" ]]
 then find "$input_dir" -type f -exec cp {} "$output_dir" \;
 else 
-     current_depth=1
-     while [[ $current_depth -le $max_depth ]]; do
-          for item in "$input_dir"/*; do
-             if [[ -d "$item" ]]
-             then dir_name=$(basename "$item")
-                  mkdir -p "$output_dir/$dir_name"
-             elif [[ -f "$item" ]]
-             then cp "$item" "$output_dir/"
-             fi
-             done
-          input_dir="$input_dir/*"
-          current_depth=$((current_depth + 1))
-        done
-fi
+     copy $input_dir 0
+    
